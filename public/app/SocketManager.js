a7477c56e24ffbd646d3218a2b53f28c154959b9
Hargobind S. Khalsa <khalsah@gmail.com>
2011-05-14 11:55:46 -0600

    Cleanup SocketManager

--- public/app/SocketManager.js
+++ public/app/SocketManager.js
@@ -1,21 +1,29 @@
 Ext.define('BLP2.SocketManager', {
   singleton: true,
 
+  constructor: function(){
+    this.sockets = {};
+    return this;
+  },
+
   connect: function(scope, host, options){
     var scope = scope || "default";
-    if(typeof this.sockets == 'undefined') { this.sockets = {}; }
-    if(typeof this.sockets[scope] != 'undefined') { return this.sockets[scope]; }
+    if(this.sockets.hasOwnProperty()) { return this.sockets[scope]; }
+
     var socket;
     socket = new io.Socket(host, options);
     socket.connect();
-    socket.on('message', function(data){ console.log(data.body); });
     this.sockets[scope] = socket;
+
+    socket.on('message', function(data){ console.log(data.body); });
+
     return socket;
   },
 
   disconnect: function(scope){
     var scope = scope || "default";
-    if(typeof this.sockets[scope] == 'undefined') { return; }
+    if(!this.sockets.hasOwnProperty()) { return; }
+
     this.sockets[scope].disconnect();
     delete this.sockets[scope];
   }
