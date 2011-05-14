519596de37ecbd4d8e40057b48b60b88aab9d558
Hargobind S. Khalsa <khalsah@gmail.com>
2011-05-13 21:09:21 -0600

    Add ExtJS Hello World Application

---
+++ public/app.js
@@ -0,0 +1,11 @@
+Ext.Loader.setConfig({
+  enabled: true,
+  paths: {'Ext': '/extjs/src'}
+});
+
+Ext.application({
+  name: 'BLP2',
+  autoCreateViewport: true,
+  controllers: [],
+  launch: function(){}
+});
---
+++ public/app/view/Viewport.js
@@ -0,0 +1,9 @@
+Ext.define('BLP2.view.Viewport', {
+  extend: 'Ext.container.Viewport',
+
+  layout: 'fit',
+  items: [{
+    xtype: 'panel',
+    title: 'Hello World!'
+  }]
+});
